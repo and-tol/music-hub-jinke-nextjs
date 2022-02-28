82d05ac1df59140653c1e2bfc9603a9c665e37af
Andrei Tolstoi <tolstoi.a@gmail.com>
2022-02-28 20:35:07 +0200

    feat: implement basic project settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { AppView } from '../views/app';
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Music Hub</title>
-        <meta name='description' content='Hub for your loved music' />
-        <link rel='icon' href='/favicon.svg' />
-      </Head>
-
       <AppView
         header={<div>Header</div>}
         content={<div>Content</div>}
