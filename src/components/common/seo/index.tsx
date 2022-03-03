d19dd060b7990524b5bdcf18960ad9ef57e8b36a
Andrei Tolstoi <tolstoi.a@gmail.com>
2022-03-03 12:30:52 +0200

    refact: seo

---
+++ src/components/common/seo/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import { ReactElement } from 'react';
+
+type PropsType = {
+  description?: string;
+  siteTitle: string;
+  pageTitle?: string;
+};
+
+const config = Object.freeze({
+  social: { twitter: 'twitter' },
+});
+
+export default function SEOComponent({
+  description,
+  siteTitle,
+  pageTitle,
+}: PropsType): ReactElement {
+  return (
+    <Head>
+      <title>{`${siteTitle} | ${pageTitle}`}</title>
+      <meta name='description' content={description} />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content={siteTitle} />
+      <meta property='og:description' content={description} />
+      <meta property='og:site_name' content={pageTitle} />
+      <meta property='twitter:card' content='summary' />
+      <meta property='twitter:creator' content={config.social.twitter} />
+      <meta property='twitter:title' content={siteTitle} />
+      <meta property='twitter:description' content={description} />
+    </Head>
+  );
+}
